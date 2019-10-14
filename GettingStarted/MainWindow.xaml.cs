5135013988ba6143bed1953c41ec103c8fcc3f9c
Mohanraj-Guna <45057763+Mohanraj-Guna@users.noreply.github.com>
2019-10-14 17:53:27 +0530

    Add files via upload
    Getting started samples are committed.

---
+++ GettingStarted/MainWindow.xaml.cs
@@ -0,0 +1,43 @@
+﻿using Syncfusion.UI.Xaml.Schedule;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Threading;
+
+namespace GettingStarted
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            ScheduleAppointmentCollection appointmentCollection = new ScheduleAppointmentCollection();
+            //Creating new event   
+            ScheduleAppointment clientMeeting = new ScheduleAppointment();
+            DateTime currentDate = DateTime.Now;
+            DateTime startTime = new DateTime(currentDate.Year, currentDate.Month, currentDate.Day, 10, 0, 0);
+            DateTime endTime = new DateTime(currentDate.Year, currentDate.Month, currentDate.Day, 12, 0, 0);
+            clientMeeting.StartTime = startTime;
+            clientMeeting.EndTime = endTime;
+            clientMeeting.AppointmentBackground = Brushes.Blue;
+            clientMeeting.Subject = "ClientMeeting";
+            appointmentCollection.Add(clientMeeting);
+            schedule.Appointments = appointmentCollection;
+           
+        }
+    }
+}
